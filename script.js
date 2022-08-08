c4c1c868e26f4d4c0d626574670568be9686c899
aesh9 <ar50gyn13@gmail.com>
2022-08-08 18:52:39 -0300

    FashionistaHome

--- script.js
+++ script.js
@@ -11,3 +11,51 @@ cartIcon.onclick = () =>{
   closeCart.onclick=()=>{
     cart.classList.remove("active");
   };
+
+  /* SLIDE */
+var index = 0
+var i = 0
+var slider = document.getElementsByClassName("slider")
+var line = document.getElementsByClassName("line")
+var tempo = 0
+
+auto()
+
+function show(n) {
+  for (let i = 0; i < slider.length; i++) { //slider.legth
+    slider[i].style.display = "none"
+  }
+  for (let i = 0; i < line.length; i++) { //line.leght
+    line[i].className = line[i].className.replace("active")
+  }
+  slider[n - 1].style.display = ("block")
+  line[n - 1].className += " active" //add space
+}
+
+
+function auto() {
+  index++
+  if (index > slider.length) { // to move from first slide
+    index = 1
+  }
+
+  show(index) // calling show
+  setTimeout(auto, 4000) // next slide will apper after 5s
+}
+
+function plusSlide(n) {
+  index += n // n=1 or n=-1
+  if (index > slider.length) { // to move from first slide
+    index = 1
+  }
+
+  if (index < 1) {
+    index = slider.length
+  }
+  show(index)
+}
+
+function correntSlide(n) {
+  index = n
+  show(index)
+}
